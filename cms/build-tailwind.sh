8917956adf4e3535889e6522137341cef0d0b30a
Cem Aydin <cem.aydin@gmx.ch>
2025-09-18 16:10:05 +0200

    use deno for package management, rebuilt css

---
+++ cms/build-tailwind.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Build Tailwind CSS and watch for changes
+#
+npx tailwindcss -o ./static/tailwind.css --watch
--- website/server.ts
+++ website/server.ts
@@ -1,18 +1,20 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
+import process from "node:process";
 
-import databaseApp from './database.ts';
-import { createLanguageApp } from './language/language.ts';
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-import { createEndpoints } from './routes.ts';
+import databaseApp from "./database.ts";
+import { createLanguageApp } from "./language/language.ts";
+
+import { createEndpoints } from "./routes.ts";
 
 const PORT: number = process.env.PORT ? Number(process.env.PORT) : 3002;
 
 const app: express.Application = express();
 
 // static files
-app.use('/static', express.static('static/'));
+app.use("/static", express.static("static/"));
 
 // modules for parsing cookies and url encoded bodies
 // NOTE: cookie-parser and body-parser are needed by subsequent apps, but should only be loaded once
@@ -33,7 +35,7 @@ app.use(databaseApp);
 // this provides capabilities and for switching language on the site,
 // the selected language is added to the request object as `req.lang`
 // NOTE: the admin app uses a separate instance of the language app
-const languageApp = createLanguageApp('lang', '/set-lang');
+const languageApp = createLanguageApp("lang", "/set-lang");
 app.use(languageApp);
 
 // below pages are rendered using the base template from './templates/base.js'
@@ -41,14 +43,14 @@ app.use(languageApp);
 // create static endpoints from directory structure
 // this recursively creates endpoints for each 'page.js' file
 // in the './routes' directory and its subdirectories.
-// 
+//
 // e.g.:
 // routes/page.js                 -> /
 // routes/about/page.js           -> /about
 // routes/contact/page.js         -> /contact
 // routes/services/repair/page.js -> /services/repair
-// 
-createEndpoints(app, './routes');
+//
+createEndpoints(app, "./routes");
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
